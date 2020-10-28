0593d819d7d86be09001b0cc2f7b3374d89bc4e7
Larry McCay <larm2112@Larrys-MBP.home>
2020-10-28 01:26:39 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LittleBabyDylan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch s = new Stopwatch();
+            s.Start();
+
+            while (s.Elapsed < TimeSpan.FromDays(60))
+            {
+                Twitter.RegisterPullTime();
+                Twitter.CheckForTweets();
+            }
+
+            s.Stop();
+        }
+    }
+}
---
+++ Twitter.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Threading;
+using System.Collections;
+using System.Globalization;
+using Tweetinvi;
+
+
+namespace LittleBabyDylan
+{
+    public class Twitter
+    {
+        static DateTime pullTime;
+
+        public static void CheckForTweets()
+        {
+            Auth.SetUserCredentials("YOUR TWITTER API TOKENS HERE");
+            var user = User.GetAuthenticatedUser();
+
+            long dylanID = //User's specific Twitter account ID number;
+            var dylan = User.GetUserFromId(dylanID);
+
+            var timelineParameter = Timeline.CreateUserTimelineParameter();
+            timelineParameter.ExcludeReplies = true;
+            timelineParameter.IncludeRTS = false;
+
+            var lastTweets = dylan.GetUserTimeline(timelineParameter);
+            
+            foreach (var tweet in lastTweets) 
+            {
+                TimeSpan mins = TimeSpan.FromMinutes(4);
+                if (tweet.CreatedAt >= pullTime.Subtract(mins)) 
+                {
+                    if (tweet.Text.ToString().Contains("r") || tweet.Text.ToString().Contains("R") || tweet.Text.ToString().Contains("l") || tweet.Text.ToString().Contains("L"))
+                    { 
+                        var culture = new CultureInfo("en-US");
+                        string editedTweet = tweet.Text.ToString().Replace("r", "w", true, culture).Replace("l", "w", true, culture);
+                        
+                        if (!(string.IsNullOrEmpty(editedTweet)))
+                        {
+                            Tweet.FavoriteTweet(tweet.Id);
+                            Tweet.PublishTweetInReplyTo("@" + tweet.CreatedBy.ScreenName + " " + editedTweet, tweet.Id);
+                            Console.WriteLine("Tweeted: " + editedTweet);
+                        }
+                    }
+                }
+                else if (tweet.CreatedAt < pullTime) 
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(DateTime.Now + " - Checking again...");
+        }
+
+        public static void RegisterPullTime() 
+        {
+            pullTime = DateTime.Now;
+
+            //This sleeps for 20 minutes to give him time to tweet
+            Thread.Sleep(1200000);
+        }
+
+    }
+}
